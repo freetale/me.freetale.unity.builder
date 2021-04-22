fa301ac14f96192066f892ba28f4cb3606b362ca
Free <lifeistfree@gmail.com>
2021-04-22 23:19:10 +0700

    update lib hilight message

--- Assets/me.freetale.unity.builder/Script/lib.ps1
+++ Assets/me.freetale.unity.builder/Script/lib.ps1
@@ -1,9 +1,16 @@
 
-$CONOLSE_COLOR="$([char]27)[30m$([char]27)[42m"
-$CONSOLE_RESTORE="$([char]27)[0m"
+
+function Write-Text {
+    param (
+        $Message
+    )
+    $CONOLSE_COLOR="$([char]27)[30m$([char]27)[42m"
+    $CONSOLE_RESTORE="$([char]27)[0m"
+    echo "${CONOLSE_COLOR}${Message}${CONSOLE_RESTORE}"
+}
 
 if ($UNITY_EDITOR_PATH -eq $null) {
-    echo "missing UNITY_EDITOR_PATH"
+    Write-Text "missing UNITY_EDITOR_PATH"
     exit(1)
 }
 
@@ -13,9 +20,9 @@ if ($PROJECT_PATH -eq $null) {
 
 $ARGS = @()
 if ($BUILD_TARGET -eq $null) {
-    echo "BUILD_TARGET not define, ignore"
+    Write-Text "BUILD_TARGET not define, ignore"
     if ($CACHE_NAME -eq $null -and $CACHE_PATH -eq $null) {
-        echo "missing CACHE_NAME and BUILD_TARGET"
+        Write-Text "missing CACHE_NAME and BUILD_TARGET"
         exit(1)
     }
 } else {
@@ -23,7 +30,7 @@ if ($BUILD_TARGET -eq $null) {
     $ARGS = $ARGS + "-buildTarget" + $BUILD_TARGET
     if ($CACHE_NAME -eq $null) {
         $CACHE_NAME="Cache/$BUILD_TARGET"
-        echo "missing CACHE_NAME set as ${BUILD_TARGET}"
+        Write-Text "missing CACHE_NAME set as ${BUILD_TARGET}"
     }
 }
 
@@ -40,11 +47,11 @@ if ($TEMP_CACHE_PATH -eq $null) {
 }
 
 if (-not $(Test-Path "${PROJECT_PATH}/Assets")) {
-    echo "Assets path not exist, are PROJECT_PATH point to project root? current is ${PROJECT_PATH}"
+    Write-Text "Assets path not exist, are PROJECT_PATH point to project root? current is ${PROJECT_PATH}"
     exit(1)
 }
 
-echo "${CONOLSE_COLOR}swaping library${CONSOLE_RESTORE}"
+Write-Text "swaping library"
 if (-not $(Test-Path "${PROJECT_PATH}/Cache")) {
     mkdir "${PROJECT_PATH}/Cache"
 }
@@ -52,13 +59,12 @@ if (-not $(Test-Path "${PROJECT_PATH}/Cache")) {
 mv "${PROJECT_PATH}/Library" "$TEMP_CACHE_PATH" -ErrorAction SilentlyContinue
 mv "${CACHE_PATH}" "${PROJECT_PATH}/Library" -ErrorAction SilentlyContinue
 
-echo "${CONOLSE_COLOR}Executing unity editor${CONSOLE_RESTORE}"
+Write-Text "Executing unity editor"
 &$UNITY_EDITOR_PATH -quit -batchmode -executeMethod "FreeTale.Unity.Builder.Builder.BuildMain" -logFile - -projectPath "$PROJECT_PATH" $ARGS ${EXTRA_ARGS} | Out-Host
 $UNITY_EXIT_CODE="$LASTEXITCODE"
 
-echo "${CONOLSE_COLOR}swaping library back${CONSOLE_RESTORE}"
+Write-Text "swaping library back"
 mv "${PROJECT_PATH}/Library" "${CACHE_PATH}" -ErrorAction SilentlyContinue
 mv "$TEMP_CACHE_PATH" "${PROJECT_PATH}/Library" -ErrorAction SilentlyContinue
 
-echo "finalized build with exit code ${UNITY_EXIT_CODE}"
-
+Write-Text "finalized build with exit code ${UNITY_EXIT_CODE}"
